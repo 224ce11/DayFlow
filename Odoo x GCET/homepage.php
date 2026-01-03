92bf71a00d0d2bde9a0fcea583c316b67a9457d0
shlokkharva <123727051+shlokkharva@users.noreply.github.com>
2026-01-03 11:05:06 +0530

    Fix HTML encoding issues in homepage.php

--- Odoo x GCET/homepage.php
+++ Odoo x GCET/homepage.php
@@ -7,6 +7,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600;700&family=Inter:wght@400;500;600&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="style.js">
 </head>
 <body>
 
@@ -160,3 +161,4 @@
 
 </body>
 </html>
+
